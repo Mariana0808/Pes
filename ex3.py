0b1176777aec0dba7afdc744e76656366b816fac
MachadoWD <augustodavidmachado@gmail.com>
2026-06-09 15:50:47 -0300

    mariana

---
+++ ex3.py
@@ -0,0 +1,6 @@
+usuario = input("digita usuario")
+senha = int (input("digita senha"))
+if usuario == "adimin" and senha == 12345:
+    print("Login bem-sucedido")
+else:
+    print("Nome de usuário ou senha incorretos")
